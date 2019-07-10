c680ffbbce6d0fbafd2a5a89e41eca4aa2be5a0f
Mohnish Devadiga <mohnish226@yahoo.co.in>
2019-07-10 13:58:20 +1000

    Update Chatbot

--- Chatbot1.py
+++ Chatbot1.py
@@ -24,19 +24,50 @@ import math
 USE_CUDA = torch.cuda.is_available()
 device = torch.device("cuda" if USE_CUDA else "cpu")
 
-FILE_path = "data/cornell movie-dialogs corpus/"
+#FILE_path = "data/cornell movie-dialogs corpus/"
 
 FILE_lines = "data/cornell movie-dialogs corpus/movie_lines.txt"
 FILE_convo = "data/cornell movie-dialogs corpus/movie_conversations.txt"
 
 line_fields = ["lineID","CharacterID","MovieID","character","text"]
 lines = {}
-
+l = []
 with open(FILE_lines,'rb') as f:
     for line in f:
-        value = str(line.strip()).split("+++$+++")
+        tempL = line
+        try:
+            if type(line) == type(b"123"):
+                tempL = line.decode('utf-8')
+        except Exception as e:
+            x = []
+            x.append(tempL)
+            x.append(e)
+            l.append(x)
+        value = str(tempL.strip()).split("+++$+++")
         lineObj = {}
         for i,field in enumerate(line_fields):
-            lineObj[field] = value[i]
+            lineObj[field] = ' '.join(value[i].split())
         lines[lineObj["lineID"]] = lineObj
+        
+convo_fields = ["Character1ID","Character2ID","MovieID","UtteranceID"]
+convo = {}
+
+with open(FILE_convo,'rb') as f:
+    for line in f:
+        values = str(line.strip()).split(" +++$+++ ")
+        convObj = {}
+        for i, field in enumerate(convo_fields):
+            convObj[field] = values[i]
+        #print(convObj["UtteranceID"])
+        temp = eval("\""+convObj["UtteranceID"])
+        lineIds = eval(temp)
+        convObj["lines"] = []
+        for lineId in lineIds:
+            try:
+                convObj["lines"].append(lines[lineId])
+            except:
+                try:
+                    convObj["lines"].append(lines[lineId+" "])
+                except Exception as e:
+                    print(e)
         
